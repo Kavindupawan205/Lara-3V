839b7b0c4ed7ca8f29a4fd2c934e484d3a1e8f15
Kavindupawan205 <129190793+Kavindupawan205@users.noreply.github.com>
2025-08-29 19:59:01 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "6YoDUBqT#VBBoQSyTdjK472smsHul_LJSYjZ1jWJIpxsz8SZg5vo", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "rNwR0RIL#jqpHI4gZCwDWCRfHcZD3y9PR5Z-nEbfyvxJaMZFphAY", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94750313394",
